77077e412a512b32c7c120185001394c9d964290
samgarthwaite <43002471+samgarthwaite@users.noreply.github.com>
2018-11-02 08:34:04 -0400

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author Sam Garthwaite
+ *
+ */
+public class homework 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My Favorate Subjects are\n\t English \n\t Math \n\t History");
+	}
+}
