f0c6401cf8c640090941114ed6d976c2fab37862
samgarthwaite <43002471+samgarthwaite@users.noreply.github.com>
2018-11-16 18:10:45 -0500

    Add files via upload

---
+++ VariableTest.java
@@ -0,0 +1,13 @@
+package ugh;
+
+public class VariableTest 
+{
+	public static void main(String[] args)
+	{
+		char char1 = 'b';
+		char char2 = 'c';
+		int num1 = 1;
+		System.out.println("My first char is " + char1 + ", my second variable is " + char2 + "My third variable is " + num1);
+		
+	}
+}
